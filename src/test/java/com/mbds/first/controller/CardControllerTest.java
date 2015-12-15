4be2afff34985e0b354ad6927fbfbeff736242d6
marwensaid <marwensaidi1@gmail.com>
2015-12-15 10:29:54 +0100

    feat(backEnd-First) test terminal list & refactor pom (add assertJ)-- MS

---
+++ src/test/java/com/mbds/first/controller/CardControllerTest.java
@@ -0,0 +1,44 @@
+package com.mbds.first.controller;
+
+import com.mbds.first.domain.ResultWrapperGuava;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import javax.smartcardio.CardException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+/**
+ * Created by marwen on 15/12/15.
+ */
+
+@RunWith(MockitoJUnitRunner.class)
+public class CardControllerTest{
+
+    private static final String TERMINALS = "";
+
+    @InjectMocks
+    private CardController tested;
+
+    @Mock
+    private CardController cardControllerMock;
+
+    @Test
+    public void testTerminalList() throws CardException {
+        // given
+        when(cardControllerMock.terminalList()).thenReturn(testTerminalList(TERMINALS));
+
+        //when
+        ResultWrapperGuava actual = tested.terminalList();
+
+        //then
+        assertThat(actual).isNotNull();
+    }
+
+
+}
