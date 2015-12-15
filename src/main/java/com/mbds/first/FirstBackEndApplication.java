b0b6d40782d3c30733ea0743de6e7414d1e7568a
marwensaid <marwensaidi1@gmail.com>
2015-12-15 09:30:41 +0100

    feat(backEnd-First) add project struct -- MS

---
+++ src/main/java/com/mbds/first/FirstBackEndApplication.java
@@ -0,0 +1,12 @@
+package com.mbds.first;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstBackEndApplication.class, args);
+    }
+}
---
+++ src/test/java/com/mbds/first/FirstBackEndApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mbds.first;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = FirstBackEndApplication.class)
+public class FirstBackEndApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
