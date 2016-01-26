fc4f836863548cc04409461cad571ecb59fa4cb4
BENATHMANE <ayoub.benathmane@usmba.ac.ma>
2016-01-26 21:11:40 +0100

    feat(meCoin) Initialize new window -- AB

--- src/org/mbds/wolf/java/view/TerminalAddUserJFrame.java
+++ src/org/mbds/wolf/java/view/TerminalAddUserJFrame.java
@@ -7,24 +7,24 @@ import java.awt.*;
  * Created by BENATHMANE on 26/01/2016.
  */
 public class TerminalAddUserJFrame extends JFrame {
-    private JPanel container = new JPanel();
-    private JComboBox combo = new JComboBox();
-    private JLabel label = new JLabel("Une ComboBox");
+    public static String title = "meCoin Terminal";
+    private JPanel mainPanel = new JPanel();
 
     public TerminalAddUserJFrame() {
-        this.setTitle("Animation");
-        this.setSize(300, 300);
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setLocationRelativeTo(null);
-        container.setBackground(Color.white);
-        container.setLayout(new BorderLayout());
-        combo.setPreferredSize(new Dimension(100, 20));
 
-        JPanel top = new JPanel();
-        top.add(label);
-        top.add(combo);
-        container.add(top, BorderLayout.NORTH);
-        this.setContentPane(container);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setTitle(title);
+        setSize(500, 500);
+        setResizable(true);
+        setLocationRelativeTo(null);
+        add(mainPanel);
+        GridBagLayout gridBagLayout = new GridBagLayout();
+        gridBagLayout.columnWidths = new int[] {30, 30, 30, 30, 30, 30, 30, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+        gridBagLayout.rowHeights = new int[] {30, 0, 50, 30, 0, 30, 30, 30, 30, 30, 30};
+        gridBagLayout.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0, Double.MIN_VALUE, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
+        gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0};
+        mainPanel.setLayout(gridBagLayout);
+
         this.setVisible(true);
     }
 }
